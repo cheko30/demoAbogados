71b5b9db091904f678ac6ff72d9eb5a4aa689eb7
Sergio <sergio.pena.orozco@gmail.com>
2018-02-28 15:22:55 -0600

    Mejoras

--- js/databaseManager.js
+++ js/databaseManager.js
@@ -287,5 +287,6 @@ function deleteContrato(id)
     var element =document.getElementById("row_"+key);
     element.parentElement.removeChild(element);
     databaseManager.Contratos.getById(key,function(element){element.delete();window.location = "contratos.html"});
+    
 }
 
